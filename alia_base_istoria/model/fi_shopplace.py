a6c411edfeb644869caa413e113de45ae6da4fef
castor <castorsanchez@alialabs.com>
2018-08-22 04:16:54 +0200

    Fix on shops

--- alia_base_istoria/model/fi_shopplace.py
+++ alia_base_istoria/model/fi_shopplace.py
@@ -13,6 +13,6 @@ class fi_shopplace(models.Model):
     place_id = fields.Many2one('fi.place', 'Place', required=True, ondelete="cascade", select=True, auto_join=True)
     partner_id = fields.Many2one('res.partner',string = "Owner", required=True)
     type = fields.Selection([('artisan','Artisan'),('fixed','Fixed'),('draw','Draw')])
-    state = fields.Selection([('draft','Draft'),('contract','Contract'),('active','Active'),('closed','Closed')],default='activo')
+    state = fields.Selection([('draft','Draft'),('contract','Contract'),('active','Active'),('closed','Closed')],default='draft')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
